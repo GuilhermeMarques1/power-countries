ba873495d6de8f6c803177f647cecc13b0975e8d
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-08 02:37:15 -0300

    feat: Add country view

--- src/components/CountriesList/index.tsx
+++ src/components/CountriesList/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import RestCountriesAPI from "../../services/RestCountriesAPI";
 
 import { Container, Content, CountryContent, Title, Info } from "./styles";
@@ -7,6 +8,7 @@ import { Container, Content, CountryContent, Title, Info } from "./styles";
 import { ICountriesList } from "../../Types/ICountries";
 
 export function CountriesList() {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
   const [countries, setCountries] = useState<ICountriesList[]>([]);
 
@@ -29,7 +31,10 @@ export function CountriesList() {
       <Content>
         {
           countries.map((country) => (
-            <CountryContent key={country.name.official}>
+            <CountryContent 
+              onClick={() => navigate('/country')}
+              key={country.name.official}
+            >
               <img src={country.flags.svg} alt={country.flags.alt} />
               <div>
                 <Title>{country.translations.por.common.toLocaleUpperCase()}</Title>
--- src/route.tsx
+++ src/route.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { CountryView } from "./pages/CountryView";
 import { History } from "./pages/History";
 
 export function AppRoutes() {
@@ -9,6 +10,7 @@ export function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
+        <Route path="/country" element={<CountryView />} />
       </Routes>
     </BrowserRouter>
   );
